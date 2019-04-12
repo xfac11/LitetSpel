991e16bb4089901293c116dfe2dee66b448c400f
Jennifer L-S <jenny-katt@hotmail.com>
2019-04-12 03:12:24 +0200

    ModelLoader class created (mostly empty)

---
+++ OnGoingEngine/AnimalCarnage/ModelLoader.cpp
@@ -0,0 +1,23 @@
+#include "ModelLoader.h"
+
+ModelLoader::ModelLoader()
+{
+}
+
+
+ModelLoader::~ModelLoader()
+{
+}
+
+void ModelLoader::loadModel(Model& model, const char* filePath)
+{
+	//Luna::Reader reader;
+	//reader.readFile(filePath);
+
+	//unsigned int meshCount = reader.getMeshCount();
+
+	//std::vector<Luna::Vertex> vertices;
+
+	//model.setMesh(vertices, indices, mesh.indexCount);
+	//model.setTexture();
+}
---
+++ OnGoingEngine/AnimalCarnage/ModelLoader.h
@@ -0,0 +1,19 @@
+#ifndef MODELLOADER_H
+#define MODELLOADER_H
+#include <fstream>
+#include "Model.h"
+//#include "Luna/Luna.h"
+//#include "Luna/Reader.h"
+class ModelLoader
+{
+public:
+	ModelLoader();
+	~ModelLoader();
+
+	void loadModel(Model &model, const char* filePath);
+
+private:
+
+};
+
+#endif // !MODELLOADER_H
