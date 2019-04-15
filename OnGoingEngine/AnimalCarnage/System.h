62b242cd9e0e95b2408c26b2c80c17bfa546fbd1
Redx93 <38983714+Redx93@users.noreply.github.com>
2019-04-15 07:19:16 -0700

    Add files via upload

--- OnGoingEngine/AnimalCarnage/System.h
+++ OnGoingEngine/AnimalCarnage/System.h
@@ -3,13 +3,12 @@
 #include <math.h>
 #include <d3d11.h>
 #include <DirectXMath.h>
-#include "GraphicsDevice.h"
-#include "ForwardShader.h"
-#include "GameObject.h"
-#include "Camera.h"
-#include "Keyboard.h"
-#include "Mouse.h"
-#include "InputHandler.h"
+#include"GraphicsDevice.h"
+#include"ForwardShader.h"
+#include"GameObject.h"
+#include"Camera.h"
+#include"Keyboard.h"
+#include"Mouse.h"
 #include <string>
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_win32.h"
@@ -19,8 +18,8 @@
 using namespace DirectX;
 //using namespace DirectX::SimpleMath;
 
-#define HEIGHT 1000
-#define WIDTH 1000
+#define HEIGHT 768.0f
+#define WIDTH 768.0f
 //using Microsoft::WRL::ComPtr;
 
 enum GameState {
@@ -53,10 +52,6 @@ private:
 	static GameState currentState;
 
 	GameObject* obj;
-	GameObject* playerOne;
-	GameObject* playerTwo;
-	int currentInput; //imgui, controlls input
-
 	ForwardShader* theForwardShader;
 	Camera* theCamera;
 	Direction forward; //have these in a own class?
@@ -64,23 +59,12 @@ private:
 	Direction up_down;
 	void move(Direction forward, Direction left_right, Direction up_down, bool flyMode, int mouseX, int mouseY);
 	
-	
-	InputHandler* playerInputs;
 
-	//imgui variables
-	DirectX::XMFLOAT3 camPos;
-	DirectX::XMFLOAT3 camRot;
-	bool guiCam;
-
-
-	bool mouseShow;
 	bool mouseSwitch;
 	bool flySwitch;
 	bool moveScreen;
-	bool isPressed;
 
 	void initImgui();
-	void renderImgui();
 	void update(float deltaTime);
 	void render();
 
