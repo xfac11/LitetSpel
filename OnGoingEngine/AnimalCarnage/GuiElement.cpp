c91d5388edc9561ca61495877cc1688bd566b6f5
mrbengtsson <viktor.bengtsson98@gmail.com>
2019-04-17 14:39:45 +0200

    Gui Element added, connections between elements

---
+++ OnGoingEngine/AnimalCarnage/GuiElement.cpp
@@ -0,0 +1,52 @@
+#include "GuiElement.h"
+
+GuiElement::GuiElement(DirectX::SimpleMath::Vector2 position) : position(position), left(nullptr), right(nullptr), up(nullptr), down(nullptr)
+{
+}
+
+GuiElement::~GuiElement()
+{
+}
+
+bool GuiElement::update(float deltaTime)
+{
+	return true;
+}
+
+bool GuiElement::render(DirectX::SpriteBatch * spriteBatch)
+{
+	return true;
+}
+
+void GuiElement::setConnectedElements(GuiElement * left, GuiElement * right, GuiElement * up, GuiElement * down)
+{
+	this->left = left;
+	this->right = right;
+	this->up = up;
+	this->down = down;
+}
+
+DirectX::SimpleMath::Vector2 GuiElement::getPosition() const
+{
+	return this->position;
+}
+
+GuiElement * GuiElement::getLeft() const
+{
+	return this->left;
+}
+
+GuiElement * GuiElement::getRight() const
+{
+	return this->right;
+}
+
+GuiElement * GuiElement::getUp() const
+{
+	return this->up;
+}
+
+GuiElement * GuiElement::getDown() const
+{
+	return this->down;
+}
---
+++ OnGoingEngine/AnimalCarnage/GuiElement.h
@@ -0,0 +1,34 @@
+#ifndef GUI_ELEMENT_H
+#define GUI_ELEMENT_H
+
+#include "SpriteBatch.h"
+#include <d3d11.h>
+#include "SimpleMath.h"
+#include <functional>
+
+class GuiElement
+{
+protected:
+	DirectX::SimpleMath::Vector2 position;
+	GuiElement* left;
+	GuiElement* right;
+	GuiElement* up;
+	GuiElement* down;
+
+public:
+	GuiElement(DirectX::SimpleMath::Vector2 position);
+	virtual ~GuiElement();
+
+	virtual bool update(float deltaTime);
+	virtual bool render(DirectX::SpriteBatch* spriteBatch);
+
+	void setConnectedElements(GuiElement* left, GuiElement* right, GuiElement* up, GuiElement* down);
+
+	DirectX::SimpleMath::Vector2 getPosition() const;
+	GuiElement* getLeft() const;
+	GuiElement* getRight() const;
+	GuiElement* getUp() const;
+	GuiElement* getDown() const;
+};
+
+#endif // !GUI_ELEMENT_H
