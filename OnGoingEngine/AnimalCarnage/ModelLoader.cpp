3ad268139d61afe83eb36bcb96c4b3d59eab347e
xfac11 <xfac115@gmail.com>
2019-05-25 01:55:58 +0200

    Already loaded in model fix

--- OnGoingEngine/AnimalCarnage/ModelLoader.cpp
+++ OnGoingEngine/AnimalCarnage/ModelLoader.cpp
@@ -61,7 +61,8 @@ void ModelLoader::loadGO(GameObject*& object, const char* filePath, int mipLevel
 			
 
 			object->addModel(m, mesh.hasSkeleton); //
-			object->setHalfSize(reader.getBoundingBox(0).halfSize, reader.getBoundingBox(0).pos);
+			if (mesh.hasBoundingBox)
+				object->setHalfSize(reader.getBoundingBox(0).halfSize, reader.getBoundingBox(0).pos);
 			return;
 		}
 
