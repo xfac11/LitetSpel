149bcd74ef8bb0140ce78062ecbad630e9234a21
jabbathefrukt <nilsdaniellagula@gmail.com>
2019-05-30 20:16:03 +0200

    Updates
    - Main_Theme_2.0
    - Bullet deltaTime fix?
    - Hitbox Scale

--- OnGoingEngine/AnimalCarnage/Player.cpp
+++ OnGoingEngine/AnimalCarnage/Player.cpp
@@ -172,12 +172,13 @@ void Player::changeCharacter()
 	}
 
 	this->setAnimalType(ArrayOfAnimals[currentAnimal]);
-	System::theModelLoader->loadAO(this->playerObj, Animal::getAnimal(ArrayOfAnimals[currentAnimal]).modelPath, Animal::getAnimal(ArrayOfAnimals[currentAnimal]).animalAnimations);
+	//////System::theModelLoader->loadAO(this->playerObj, Animal::getAnimal(ArrayOfAnimals[currentAnimal]).modelPath, Animal::getAnimal(ArrayOfAnimals[currentAnimal]).animalAnimations);
+	System::theModelLoader->loadAO(this->playerObj, Animal::getAnimal(ArrayOfAnimals[currentAnimal]).modelPath, Animal::getAnimal(ArrayOfAnimals[currentAnimal]).animalAnimations, Animal::getAnimal(ArrayOfAnimals[currentAnimal]).attackJoint);
 	System::assetMananger->LoadTexture(Animal::getAnimal(ArrayOfAnimals[currentAnimal]).maskPath, Animal::getAnimal(ArrayOfAnimals[currentAnimal]).maskPath);
 	this->playerObj->getModel()->setMaskTexture(System::assetMananger->GetTexture(Animal::getAnimal(ArrayOfAnimals[currentAnimal]).maskPath));
-	System::theModelLoader->loadAO(this->playerObj, Animal::getAnimal(ArrayOfAnimals[currentAnimal]).modelPath, Animal::getAnimal(ArrayOfAnimals[currentAnimal]).animalAnimations, Animal::getAnimal(ArrayOfAnimals[currentAnimal]).attackJoint);
-	System::assetMananger->LoadTexture(Animal::getAnimal(type).maskPath, Animal::getAnimal(type).maskPath);
-	this->playerObj->getModel()->setMaskTexture(System::assetMananger->GetTexture(Animal::getAnimal(type).maskPath));
+	//////System::assetMananger->LoadTexture(Animal::getAnimal(type).maskPath, Animal::getAnimal(type).maskPath);
+	//////this->playerObj->getModel()->setMaskTexture(System::assetMananger->GetTexture(Animal::getAnimal(type).maskPath));
+	
 	//System::theModelLoader->loadGO(this->playerObj, Animal::getAnimal(ArrayOfAnimals[currentAnimal]).modelPath);
 
 	//if (animal.maskPath != "empty" && !this->playerObj->getModel()->hasMaskColor())
